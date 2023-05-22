642f9c15e69dc9203c948f99f8375151d18d44a4
Prateek Sachan <ps@prateeksachan.com>
2023-05-22 20:15:20 +0530

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import netlify from '@astrojs/netlify/functions';
+
+
+// https://astro.build/config
+export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      theme: 'dracula',
+      wrap: true
+    }
+  },
+  integrations: [tailwind()],
+  output: "server",
+  adapter: netlify()
+});
